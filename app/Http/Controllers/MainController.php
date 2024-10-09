1193076b571aa559d7e99281595840ead08e08ae
farelogy <katsuragi.farel@gmail.com>
2024-10-09 15:32:24 +0700

    first up

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class MainController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+            'name' => 'required',
+            'c_password' => 'required'
+        ]);
+
+        if($validated)
+        {
+            $new_user = new User();
+            $new_user->name = $request->name;
+            $new_user->email = $request->email;
+            $new_user->password = Hash::make($request->password);
+            $new_user->save();
+            return response()->json([
+                'status' => 'Success',
+                'message' => 'Authenticated'
+            ],200);
+        }
+        else
+        {
+            return response([
+                'status' => 'Error',
+                'message' => "Sorry, We can't Authenticate You."
+            ], 500);
+        }
+    }
+}
