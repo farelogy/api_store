1a9aaab71e3d101792b2517eda40da2e8d0224f7
farelogy <katsuragi.farel@gmail.com>
2024-10-14 15:13:22 +0700

    update check token

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Http\Controllers\MainController;
-
+use Laravel\Sanctum\PersonalAccessToken;
+use Carbon\Carbon;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,6 +23,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::middleware(['auth:sanctum', 'check.token.expiration'])->get('/check-token', function (Request $request) {
+    $token = $request->access_token;
+        
+    if ($token) {
+        $tokenData = PersonalAccessToken::findToken($token);
+
+        if (!$tokenData || $tokenData->expires_at->lt(Carbon::now())) {
+            return response()->json(['message' => 'Token is expired'], 401);
+        }
+    } else {
+        return response()->json(['message' => 'Token not provided'], 401);
+    }
     return response()->json(['message' => 'Token is valid'], 200);
 });
 
