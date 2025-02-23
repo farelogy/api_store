a79241fc96ee4ca82e4bf63a2cbfe05d4bb49f8a
farelogy <katsuragi.farel@gmail.com>
2025-02-23 08:54:04 +0700

    add query add barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Barang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -12,6 +13,30 @@ class BarangController extends Controller
             'status' => 'Success',
             'message' => 'Data Barang diterima',
             'data' => $get_barang
+        ],200);
+    }
+
+    public function add_barang(Request $request){
+        $validated = Validator::make($request->all(), [
+            'nama_barang' => 'required',
+            'harga' => 'required',
+        ]);
+
+        if($validated->fails())
+        {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $validated->errors()
+            ], 200);
+        }
+
+        $barang = new Barang();
+        $barang->nama_barang = $request->nama_barang;
+        $barang->harga = $request->harga;
+        $barang->save();
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'Tambah Barang Berhasil',
         ],200);
     }
 }
