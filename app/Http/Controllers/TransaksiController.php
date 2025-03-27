07705caaf1fb002cbdcd532e5ed645c11d99da07
farelogy <katsuragi.farel@gmail.com>
2025-03-27 16:37:43 +0700

    tes

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -103,7 +103,7 @@ class TransaksiController extends Controller
         return response()->json([
             'status' => 'Success',
             'message' => 'Check Out Berhasil',
-            'item' => $item,
+            'item' => $request->item,
         ],200);
         //hapus keranjang
         Keranjang::where('id_cabang',$request->id_cabang)->delete();
