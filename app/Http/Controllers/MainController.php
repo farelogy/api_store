ea5a06650ba0148703874b8258fda860d67b44dd
farelogy <katsuragi.farel@gmail.com>
2025-03-28 18:41:25 +0700

    update query

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -203,7 +203,7 @@ LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = DATE(NOW())
         //delete transaksi cabang
         Transaksi::where('id_cabang',$request->id_cabang)->delete();
         //remove role cabang user
-        User::where('role', 'cabang')->update(['status' => null]);
+        User::where('role', 'cabang')->update(['role' => null]);
         //remove keranjang
         Keranjang::where('id_cabang',$request->id_cabang)->delete();
         //remove user to cabang
