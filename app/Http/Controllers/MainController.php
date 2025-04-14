e168c75aba0ab0aa4138753f768caf4799d38184
farelogy <katsuragi.farel@gmail.com>
2025-04-14 14:57:22 +0700

    update query

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -172,15 +172,15 @@ LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = DATE(NOW())
                 'message' => 'Something Went Wrong'
             ], 200);
         }
-        $cabang = DB::select("
-        SELECT a.*,b.tanggal,b.jumlah_transaksi,b.total_harga FROM cabang a LEFT JOIN (SELECT d.id_cabang,DATE_FORMAT(d.created_at,'%Y-%m-%d') as tanggal,COUNT(DISTINCT(d.nama_transaksi)) as jumlah_transaksi, SUM(d.total_harga) as total_harga FROM (SELECT a.*,b.id_barang,b.jumlah,c.harga,b.jumlah*c.harga as total_harga FROM transaksis a
-LEFT JOIN detailtransaksis b ON a.id = b.id_transaksi
-LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = DATE(".Carbon::parse($request->date).") ) d GROUP BY d.id_cabang,DATE_FORMAT(d.created_at,'%Y-%m-%d')) b ON a.id = b.id_cabang;
-        ");
+//         $cabang = DB::select("
+//         SELECT a.*,b.tanggal,b.jumlah_transaksi,b.total_harga FROM cabang a LEFT JOIN (SELECT d.id_cabang,DATE_FORMAT(d.created_at,'%Y-%m-%d') as tanggal,COUNT(DISTINCT(d.nama_transaksi)) as jumlah_transaksi, SUM(d.total_harga) as total_harga FROM (SELECT a.*,b.id_barang,b.jumlah,c.harga,b.jumlah*c.harga as total_harga FROM transaksis a
+// LEFT JOIN detailtransaksis b ON a.id = b.id_transaksi
+// LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = DATE(".Carbon::parse($request->date).") ) d GROUP BY d.id_cabang,DATE_FORMAT(d.created_at,'%Y-%m-%d')) b ON a.id = b.id_cabang;
+//         ");
         return response()->json([
             'status' => 'Success',
             'message' => 'Data Cabang diterima',
-            'data' => $cabang
+            'data' => Carbon::parse($request->date)
         ],200);
     }
 
