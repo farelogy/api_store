6afd3042ac303f98555fe77ddee71911ab60c21d
farelogy <katsuragi.farel@gmail.com>
2025-12-20 16:23:14 +0700

    update kas harian

--- app/Http/Controllers/KasharianController.php
+++ app/Http/Controllers/KasharianController.php
@@ -2,29 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use DB;
 use App\Models\Kasharian;
-use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
-use Illuminate\Database\Query\JoinClause;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class KasharianController extends Controller
 {
-    public function tambah_operasional_cabang(Request $request){
+    public function tambah_operasional_cabang(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'kategori' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' =>'Pastikan Field Input Terisi'
+                'message' => 'Pastikan Field Input Terisi',
             ], 200);
         }
 
-        $kasharian = new Kasharian();
+        $kasharian = new Kasharian;
         $kasharian->kategori = $request->kategori;
         $kasharian->keterangan = $request->keterangan;
         $kasharian->jumlah = $request->jumlah;
@@ -34,44 +32,51 @@ class KasharianController extends Controller
         $kasharian->id_cabang = $request->id_cabang;
 
         $kasharian->save();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Operasional Berhasil Ditambahkan',
-        ],200);
+        ], 200);
 
     }
 
-    public function get_operasional_cabang(Request $request){
+    public function get_operasional_cabang(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'id_cabang' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' =>'Hubungi Admin Anda'
+                'message' => 'Hubungi Admin Anda',
             ], 200);
         }
-        $kasharian = Kasharian::where('id_cabang',$request->id_cabang)->orderBy('created_at','desc')->get();
+        $kasharian = Kasharian::leftjoin('pembelis', 'kasharians.id_pembeli', '=', 'pembelis.id')
+            ->leftjoin('karyawans', 'kasharians.id_karyawan', '=', 'karyawans.id')
+            ->select('kasharians.*', 'pembelis.nama_pembeli', 'karyawans.nama_karyawan')
+            ->where('kasharians.id_cabang', $request->id_cabang)
+            ->whereDate('kasharians.created_at', Carbon::parse($request->date))
+            ->orderBy('kasharians.created_at', 'desc')->get();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Data Operasional diterima',
-            'data' => $kasharian
-        ],200);
+            'data' => $kasharian,
+        ], 200);
     }
 
-        public function edit_operasional_cabang(Request $request){
+    public function edit_operasional_cabang(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'id_kasharian' => 'required',
             'kategori' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Pastikan Field Input Terisi'
+                'message' => 'Pastikan Field Input Terisi',
             ], 200);
         }
 
@@ -84,9 +89,10 @@ class KasharianController extends Controller
         $kasharian->id_karyawan = $request->id_karyawan;
         $kasharian->id_cabang = $request->id_cabang;
         $karyawan->save();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Operasional Berhasil Diedit',
-        ],200);
+        ], 200);
     }
 }
