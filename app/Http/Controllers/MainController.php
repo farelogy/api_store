68756ccf7f7bde77b3cc01678eb64f6c34d023fa
farelogy <katsuragi.farel@gmail.com>
2025-12-29 11:25:24 +0700

    set nama tb

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User;
 use App\Models\Cabang;
 use App\Models\Keranjang;
 use App\Models\Transaksi;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
-use Laravel\Sanctum\PersonalAccessToken;
+use App\Models\User;
 use Carbon\Carbon;
 use DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
+
 class MainController extends Controller
 {
-
     public function check_token(Request $request)
     {
         $token = $request->input('access_token');
@@ -27,28 +27,27 @@ class MainController extends Controller
             return response()->json(['message' => 'Token not provided'], 401);
         }
     }
+
     public function register(Request $request)
     {
-        
+
         $validated = Validator::make($request->all(), [
             'email' => 'required|email|unique:users,email',
             'password' => 'required',
             'name' => 'required',
-        ],[
-            'email.unique' => 'Username sudah digunakan'
+        ], [
+            'email.unique' => 'Username sudah digunakan',
         ]);
-        
-        if($validated->fails())
-        {
+
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => $validated->errors()
+                'message' => $validated->errors(),
             ], 200);
         }
 
-        if($validated)
-        {
-            $new_user = new User();
+        if ($validated) {
+            $new_user = new User;
             $new_user->name = $request->name;
             $new_user->email = $request->email;
             $new_user->password = Hash::make($request->password);
@@ -56,120 +55,122 @@ class MainController extends Controller
 
             return response()->json([
                 'status' => 'Success',
-                'message' => 'Tunggu Approve oleh Admin'
-            ],200);
-        }
-        else
-        {
+                'message' => 'Tunggu Approve oleh Admin',
+            ], 200);
+        } else {
             return response([
                 'status' => 'Error',
-                'message' => "Mohon maaf, bisa mencoba register kembali."
+                'message' => 'Mohon maaf, bisa mencoba register kembali.',
             ], 500);
         }
     }
 
-    public function login(Request $request){
-        $token = "";
+    public function login(Request $request)
+    {
+        $token = '';
         $validated = Validator::make($request->all(), [
             'email' => 'required',
             'password' => 'required',
-        ],[
-            'email.required' => 'Username diperlukan'
+        ], [
+            'email.required' => 'Username diperlukan',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Pastikan Field Input Terisi'
+                'message' => 'Pastikan Field Input Terisi',
             ], 200);
         }
 
         if (! Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Unauthorized'
+                'message' => 'Unauthorized',
             ], 401);
         }
 
         $user = User::where('email', $request->email)->firstOrFail();
-        if($user->role)
-        {
+        if ($user->role) {
             $tokenResult = $user->createToken('auth_token');
             $token = $tokenResult->plainTextToken;
-    
+
             // Set expiration time to 30 days from now
             // $tokenResult->accessToken->expires_at = Carbon::now()->addDays(30);
             $tokenResult->accessToken->save();
         }
-       
 
         return response()->json([
             'name' => $user->name,
-            'role' => $user->role == null ? "kosong" : $user->role,
+            'role' => $user->role == null ? 'kosong' : $user->role,
             'status' => 'Success',
             'message' => 'Login success',
             'access_token' => $token,
-            'token_type' => 'Bearer'
+            'token_type' => 'Bearer',
         ]);
     }
 
-    public function tambah_cabang(Request $request){
+    public function tambah_cabang(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'nama_cabang' => 'required',
             'saldo' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' =>'Pastikan Field Input Terisi'
+                'message' => 'Pastikan Field Input Terisi',
             ], 200);
         }
 
-        $cabang = new Cabang();
+        $cabang = new Cabang;
         $cabang->nama_cabang = $request->nama_cabang;
         $cabang->saldo = $request->saldo;
         $cabang->save();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Cabang '.$request->nama_cabang.' Berhasil Ditambahkan',
-        ],200);
+        ], 200);
 
     }
 
-    public function data_cabang(){
+    public function data_cabang()
+    {
         $cabang = Cabang::all();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Data Cabang diterima',
-            'data' => $cabang
-        ],200);
+            'data' => $cabang,
+        ], 200);
     }
 
-    public function data_cabang_home(){
+    public function data_cabang_home()
+    {
         $cabang = DB::select("
         SELECT a.*,b.tanggal,b.jumlah_transaksi,b.total_harga FROM cabang a LEFT JOIN (SELECT d.id_cabang,DATE_FORMAT(d.created_at,'%Y-%m-%d') as tanggal,COUNT(DISTINCT(d.nama_transaksi)) as jumlah_transaksi, SUM(d.total_harga) as total_harga FROM (SELECT a.*,b.id_barang,b.jumlah,c.harga,b.jumlah*c.harga as total_harga FROM transaksis a
 LEFT JOIN detailtransaksis b ON a.id = b.id_transaksi
 LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = DATE(NOW()) ) d GROUP BY d.id_cabang,DATE_FORMAT(d.created_at,'%Y-%m-%d')) b ON a.id = b.id_cabang;
         ");
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Data Cabang diterima',
-            'data' => $cabang
-        ],200);
+            'data' => $cabang,
+        ], 200);
     }
-    public function data_cabang_home2(Request $request){
+
+    public function data_cabang_home2(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'date' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Something Went Wrong'
+                'message' => 'Something Went Wrong',
             ], 200);
         }
         $cabang = DB::select("
@@ -177,25 +178,26 @@ LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = DATE(NOW())
 LEFT JOIN detailtransaksis b ON a.id = b.id_transaksi
 LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = '".Carbon::parse($request->date)->toDateString()."') d GROUP BY d.id_cabang,DATE_FORMAT(d.created_at,'%Y-%m-%d')) b ON a.id = b.id_cabang;
         ");
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Data Cabang diterima',
-            'data' => $cabang
-        ],200);
+            'data' => $cabang,
+        ], 200);
     }
 
-    public function edit_cabang(Request $request){
+    public function edit_cabang(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'id_cabang' => 'required',
             'nama_cabang' => 'required',
             'saldo' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Pastikan Field Input Terisi'
+                'message' => 'Pastikan Field Input Terisi',
             ], 200);
         }
 
@@ -203,113 +205,113 @@ LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = '".Carbon::
         $cabang->nama_cabang = $request->nama_cabang;
         $cabang->saldo = $request->saldo;
         $cabang->save();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Cabang '.$request->nama_cabang.' Berhasil Diedit',
-        ],200);
+        ], 200);
     }
 
-    public function delete_cabang(Request $request){
+    public function delete_cabang(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'id_cabang' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Terdapat Error'
+                'message' => 'Terdapat Error',
             ], 200);
         }
         //delete stok cabang
-        DB::table('stok_barang')->where('id_cabang',$request->id_cabang)->delete();
+        DB::table('stok_barang')->where('id_cabang', $request->id_cabang)->delete();
         //delete transaksi cabang
-        Transaksi::where('id_cabang',$request->id_cabang)->delete();
+        Transaksi::where('id_cabang', $request->id_cabang)->delete();
         //remove role cabang user
-        $get_user_role = DB::table('user_to_cabang')->where('id_cabang',$request->id_cabang)->get();
-        foreach($get_user_role as $x)
-        {
+        $get_user_role = DB::table('user_to_cabang')->where('id_cabang', $request->id_cabang)->get();
+        foreach ($get_user_role as $x) {
             User::where('id', $x->id_user)->update(['role' => null]);
         }
         //remove keranjang
-        Keranjang::where('id_cabang',$request->id_cabang)->delete();
+        Keranjang::where('id_cabang', $request->id_cabang)->delete();
         //remove user to cabang
-        DB::table('user_to_cabang')->where('id_cabang',$request->id_cabang)->delete();
+        DB::table('user_to_cabang')->where('id_cabang', $request->id_cabang)->delete();
         //remove cabang
         $cabang = Cabang::find($request->id_cabang);
         $cabang->delete();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Cabang '.$request->nama_cabang.' Berhasil Dihapus',
-        ],200);
+        ], 200);
     }
 
-    public function data_user(){
-        $user = User::leftJoin('user_to_cabang','users.id','=','user_to_cabang.id_user')
-        ->leftJoin('cabang','user_to_cabang.id_cabang','=','cabang.id')
-        ->select('users.*','user_to_cabang.id_cabang','cabang.nama_cabang')->get();
+    public function data_user()
+    {
+        $user = User::leftJoin('user_to_cabang', 'users.id', '=', 'user_to_cabang.id_user')
+            ->leftJoin('cabang', 'user_to_cabang.id_cabang', '=', 'cabang.id')
+            ->select('users.*', 'user_to_cabang.id_cabang', 'cabang.nama_cabang')->get();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Data User diterima',
-            'data' => $user
-        ],200);
+            'data' => $user,
+        ], 200);
     }
 
-    public function edit_user(Request $request){
+    public function edit_user(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'id' => 'required',
             'role' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => $validated->errors()
+                'message' => $validated->errors(),
             ], 200);
         }
         $user = User::find($request->id);
         $user->role = $request->role;
-        if($request->cabang != null)
-        {
+        if ($request->cabang != null) {
             //cek apabila user sudah termap ke cabang
-            $cek_user = DB::table('user_to_cabang')->where('id_user',$request->id)->count();
-            if($cek_user == 0)
-            {
+            $cek_user = DB::table('user_to_cabang')->where('id_user', $request->id)->count();
+            if ($cek_user == 0) {
                 DB::table('user_to_cabang')->insert([
-                    'id_user'=>$request->id,
-                    'id_cabang'=>$request->cabang
+                    'id_user' => $request->id,
+                    'id_cabang' => $request->cabang,
                 ]);
-            }
-            else{
-                $delete = DB::table('user_to_cabang')->where('id_user',$request->id)->delete();
-                if($delete)
-                {
+            } else {
+                $delete = DB::table('user_to_cabang')->where('id_user', $request->id)->delete();
+                if ($delete) {
                     DB::table('user_to_cabang')->insert([
-                        'id_user'=>$request->id,
-                        'id_cabang'=>$request->cabang
+                        'id_user' => $request->id,
+                        'id_cabang' => $request->cabang,
                     ]);
                 }
-                
+
             }
         }
         $user->save();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'Informasi User '.$user->name.' Berhasil Disimpan',
-        ],200);
+        ], 200);
     }
 
-    public function delete_user(Request $request){
+    public function delete_user(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'id_user' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => $validated->errors()
+                'message' => $validated->errors(),
             ], 200);
         }
         $user = User::find($request->id_user);
@@ -317,29 +319,30 @@ LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = '".Carbon::
         $user->tokens()->delete();
 
         //remove user to cabang if ada
-        $cek_user_cabang = DB::table('user_to_cabang')->where('id_user',$request->id_user)->count();
-        if($cek_user_cabang != 0)
-        {
-            DB::table('user_to_cabang')->where('id_user',$request->id_user)->delete();
+        $cek_user_cabang = DB::table('user_to_cabang')->where('id_user', $request->id_user)->count();
+        if ($cek_user_cabang != 0) {
+            DB::table('user_to_cabang')->where('id_user', $request->id_user)->delete();
         }
 
         $user->delete();
+
         return response()->json([
             'status' => 'Success',
             'message' => 'User '.$user->name.' Berhasil Dihapus',
-        ],200);
+        ], 200);
     }
-    public function reset_password(Request $request){
+
+    public function reset_password(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'id' => 'required',
             'password' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => $validated->errors()
+                'message' => $validated->errors(),
             ], 200);
         }
         $user = User::find($request->id);
@@ -350,23 +353,23 @@ LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = '".Carbon::
         return response()->json([
             'status' => 'Success',
             'message' => 'User '.$user->name.' Berhasil Dilakukan Reset Password',
-        ],200);
+        ], 200);
     }
 
-    public function reset_password_profile(Request $request){
+    public function reset_password_profile(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'username' => 'required',
             'password' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Terdapat Error'
+                'message' => 'Terdapat Error',
             ], 200);
         }
-        $user_cek = User::where('email',$request->username.'@berkah.com')->first();
+        $user_cek = User::where('email', $request->username.'@berkah.com')->first();
         $user = User::find($user_cek->id);
 
         //reset password user
@@ -376,30 +379,31 @@ LEFT JOIN barangs c ON b.id_barang = c.id where DATE(a.created_at) = '".Carbon::
         return response()->json([
             'status' => 'Success',
             'message' => 'Password Anda Berhasil Direset',
-        ],200);
+        ], 200);
     }
 
-    public function get_user_cabang(Request $request){
+    public function get_user_cabang(Request $request)
+    {
         $validated = Validator::make($request->all(), [
             'username' => 'required',
         ]);
 
-        if($validated->fails())
-        {
+        if ($validated->fails()) {
             return response()->json([
                 'status' => 'Error',
-                'message' => 'Terdapat Error'
+                'message' => 'Terdapat Error',
             ], 200);
         }
-        $user_cek = User::where('email',$request->username.'@berkah.com')->first();
-        $cek_hub_cabang = DB::table('user_to_cabang')->where('id_user',$user_cek->id)->first();
-        $cek_cabang = Cabang::where('id',$cek_hub_cabang->id_cabang)->first();
+        $user_cek = User::where('email', $request->username.'@berkah.com')->first();
+        $cek_hub_cabang = DB::table('user_to_cabang')->where('id_user', $user_cek->id)->first();
+        $cek_cabang = Cabang::where('id', $cek_hub_cabang->id_cabang)->first();
+
         //reset password user
         return response()->json([
             'status' => 'Success',
             'message' => 'Data User Cabang Diterima',
-            'data' => [$cek_cabang->nama_cabang,$cek_hub_cabang->id_cabang]
+            'data' => [$cek_cabang->nama_cabang, $cek_hub_cabang->id_cabang, $cek_cabang->nama_tb],
 
-        ],200);
+        ], 200);
     }
 }
