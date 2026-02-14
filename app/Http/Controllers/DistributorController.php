d00eece4b24c34fa4e8744ff2b90761fdb823fe4
farelogy <katsuragi.farel@gmail.com>
2026-02-14 16:56:36 +0700

    update fix

--- app/Http/Controllers/DistributorController.php
+++ app/Http/Controllers/DistributorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Detailtransaksidistributor;
 use App\Models\Distributor;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
