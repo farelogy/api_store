1e47de096208cdb42a29b404472f7e63a379b247
farelogy <katsuragi.farel@gmail.com>
2026-02-15 14:14:19 +0700

    update fixes

--- app/Models/Historydetailtransaksidistributor.php
+++ app/Models/Historydetailtransaksidistributor.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Historydetailtransaksidistributor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'id_distributor',
+        'id_barang',
+        'nama_barang',
+        'qty',
+        'harga_satuan',
+        'tanggal',
+        'created_at',
+        'updated_at',
+    ];
 }
